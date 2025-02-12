ace71f48c54f74195e9bbd052cd25a93698224ca
CupidReborn522 <reborn522@outlook.es>
2025-02-12 17:31:15 -0400

    cambio menor de gramatica de componente

--- app.vue
+++ app.vue
@@ -1,3 +1,21 @@
+<script setup lang="ts">
+</script>
+
 <template>
-  <ProyectosTabla /> 
+  <div id="app">
+
+    <ProyectosTabla />
+    <FormularioCrearProyecto />
+  </div>
 </template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
